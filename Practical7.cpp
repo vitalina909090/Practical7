7475ac1a5aa1da1509ba82d61847d9ded33373f0
vitalina909090 <legezavitalina06@gmail.com>
2024-09-30 18:40:18 +0300

    Добавьте файлы проекта.

---
+++ Practical7.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+template <class T>
+void arithmeticMean(T array[], size_t size) {
+    double sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += array[i];
+    }
+    sum = sum / size;
+    cout << endl<< sum<< endl;
+}
+
+template <class T>
+void print(T array[], size_t size) {
+    for (int i = 0; i < size; i++) {
+        cout<< array[i]<<" ";
+    }
+}
+
+int main()
+{
+    int Array[]{ 1, 2, 3, 4 };
+    int size = sizeof(Array) / sizeof(int);
+    print <int>(Array, size);
+    arithmeticMean <int>(Array, size);
+
+}
+
